e3898626f987c76c79d3f9ca6bfc6c0f35b9b1d8
manishkumar <manish12099@gmail.com>
2026-02-16 22:27:32 +0530

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-           <h3>This is the Header component</h3>
+           <h3>This is the Header component wher is</h3>
 
         </div>
     );
